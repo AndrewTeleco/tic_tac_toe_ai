9653f76d37e362938374a6b772da96cf40889925
AndrewTeleco <andresdaglr@gmail.com>
2025-08-14 12:46:56 +0200

    Add pytest and basic test

--- tests/main.py
+++ tests/main.py
@@ -3,13 +3,12 @@ import logging
 from ..tic_tac_toe.user_config.user_credentials_gui import UserCredentialsGUI
 
 def my_callback(credentials):
-    print("Credenciales válidas:", credentials)
+    print("Valid credentials:", credentials)
     # Aquí puedes continuar con la lógica para lanzar el juego
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 if __name__ == '__main__':
-    # Asumiendo que tienes estos datos ya cargados:
     animals = {'Dragon': '🐉', 'Dolphin': '🐬', 'Tiger': '🐯'}
     colors = {'Gold': (255, 215, 0), 'Blue': (0, 0, 255)}
 
